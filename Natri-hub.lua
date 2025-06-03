4cb49fb2ed0ad5dd1f7233cb8caa12f801f0a9a6
Henz-coder <onphilong953@gmail.com>
2025-06-03 22:48:49 +0700

    Update Natri-hub.lua

--- Natri-hub.lua
+++ Natri-hub.lua
@@ -1,216 +1,485 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/vinh12eqweaws/zingmods/refs/heads/main/zingpro')))()
+Hoang°Minh°Hieu💤
+narichlorine_25700
+Đang chơi Roblox
+•
+🧐
+
+binhnguyen — 8:01 CH
+múp
+quá
+ỏ
+Nhân Xấu Trai — 8:02 CH
+Kkkk
+Mặc đồ đẹp đi ăn sinh nhật
+binhnguyen — 8:02 CH
+hôm nào ae mik làm bữa lẩu nhể
+Nhân Xấu Trai — 8:02 CH
+Đc đấy
+Kkk
+Pham Thien — 8:08 CH
+@finnn
+eee nao ong ranh bat hacl
+hack giup t win dead rial dckh
+finnn[CODE]
+ — 8:22 CH
+@Nhân Xấu Trai
+Nhân Xấu Trai — 8:22 CH
+J
+finnn[CODE]
+ — 8:23 CH
+dau r
+Nhân Xấu Trai — 8:23 CH
+Đây
+finnn[CODE]
+ — 8:23 CH
+ko voice a
+finnn[CODE]
+ — 9:16 CH
+!avatar @Pham Thien
+Carl-bot
+APP
+ — 9:16 CH
+Avatar for pthien0408
+Link as
+png | jpg | webp
+Avatar for pthien0408
+Play games on Carl-bot. Use /games list to see available games!
+binhnguyen — 9:19 CH
+@Nhân Xấu Trai
+@Pham Thien
+binhnguyen — 9:30 CH
+Hình ảnh
+chúc mừng
+Pham Thien — 9:32 CH
+ohhh
+wtfff
+finnn[CODE]
+ — 9:36 CH
+Hình ảnh
+Nhân Xấu Trai — 9:36 CH
+🙃🙃🙃
+Vcl
+Dell j đấy
+Pham Thien — 9:37 CH
+vcllll
+chiuuu
+Nhân Xấu Trai — 9:37 CH
+Tụi bây giỡn mặt à
+Nhưng anh nhân thích
+😂😂
+finnn[CODE]
+ — 9:37 CH
+hẹ hẹ
+Nhân Xấu Trai — 9:38 CH
+Đừng giỡn quá
+Vợ tao biết vợ tao ghen dooic đó
+finnn[CODE]
+ — 9:38 CH
+chia tay r mà :))
+Nhân Xấu Trai — 9:38 CH
+Vợ mới ae. Ạ
+finnn[CODE]
+ — 9:38 CH
+trai dk
+hay gais :))
+Nhân Xấu Trai — 9:38 CH
+Uk trai
+Tháng 7 vợ t dẫn t đi chơi
+finnn[CODE]
+ — 9:39 CH
+:))
+Nhân Xấu Trai — 9:39 CH
+Tháng 7 vợ t dẫn đi xem phim rồi dẫn đi ăn
+binhnguyen — 9:39 CH
+em nào đấy
+?
+finnn[CODE]
+ — 9:39 CH
+vào nhà nghỉ nữa
+Nhân Xấu Trai — 9:39 CH
+Rồi ngủ nhà vợ ôm vợ
+finnn[CODE]
+ — 9:39 CH
+thiếu bước rồi
+Nhân Xấu Trai — 9:39 CH
+Nhà vợ luôn chứ nhà nghỉ j nữa
+Kkk
+Qua ôm bóp hôn các kiểu
+finnn[CODE]
+ — 9:40 CH
+rồi 2 tthg nghịch buồi nhau à ??
+Nhân Xấu Trai — 9:40 CH
+😂😂
+binhnguyen — 9:42 CH
+https://youtu.be/F7L6-jvj_iM?si=-CBIi1WiGl1pbn_1
+YouTube
+Nam Nguyễn Trọng
+Khúc Hát Mừng Sinh Nhật [Remix] - Phan Đinh Tùng [Full HD] ...
+Hình ảnh
+Hoang°Minh°Hieu💤 — 10:29 CH
+@finnn
+cuu e vs
+finnn[CODE]
+ — 10:29 CH
+ha]\
+Hoang°Minh°Hieu💤 — 10:29 CH
+scripts lên r mà nó k ấn đc
+xem tên e
+finnn[CODE]
+ — 10:30 CH
+xem lai code
+hoac gui code
+Hoang°Minh°Hieu💤 — 10:30 CH
 
 local Window = OrionLib:MakeWindow({Name = "Natri-hub", HidePremium = false, SaveConfig = true, ConfigFolder = "dxl_bf"})
 
 local Tab = Window:MakeTab({
-	Name = "Scripts Mới",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+Name = "Scripts Mới",
+Icon = "rbxassetid://4483345998",
+Mở rộng
+message.txt
+6 KB
+a xem coi chổ nào sai k mà e âvấn ào scripts hack k đc
+@finnn e k có mở tisếng  nên k có nghe
+đang lén chơi
+finnn[CODE]
+ — 10:37 CH
+t đang tắt loa tắt mic mà cu
+Hoang°Minh°Hieu💤 — 10:37 CH
+check dùm e đi mà:(
+moẹ k có ấn đc
+finnn[CODE]
+ — 10:38 CH
+Hình ảnh
+Hoang°Minh°Hieu💤 — 10:38 CH
+sửa thành j
+finnn[CODE]
+ — 10:39 CH
+local Window = OrionLib:MakeWindow({Name = "Natri-hub", HidePremium = false, SaveConfig = true, ConfigFolder = "dxl_bf"})
+
+local Tab = Window:MakeTab({
+    Name = "Scripts Mới",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+Mở rộng
+message.txt
+8 KB
+Hoang°Minh°Hieu💤 — 10:39 CH
+mến @finnn vaiz lòng 
+finnn[CODE]
+ — 10:39 CH
+ko
+t thích con gái
+ko gay
+Hoang°Minh°Hieu💤 — 10:39 CH
+u
+finnn[CODE]
+ — 10:40 CH
+mé
+Hoang°Minh°Hieu💤 — 10:40 CH
+=)))
+finnn[CODE]
+ — 10:40 CH
+thg chó nào recommend cho t xem vid này xem đâu lòng quá
+anime tình cảm vcl
+Hoang°Minh°Hieu💤 — 10:43 CH
+còn lỗi nx
+Hình ảnh
+khi e ấn các mục khc thì nó cứ cứng đơ ra
+finnn[CODE]
+ — 10:43 CH
+vẫn lỗi à
+Hoang°Minh°Hieu💤 — 10:43 CH
+kiểu ấn mod client mà nó k hiện ý
+finnn[CODE]
+ — 10:44 CH
+tutu
+m chx có khai báo biến Lib mà
+Hoang°Minh°Hieu💤 — 10:44 CH
+là sao...
+binhnguyen — 10:45 CH
+@finnn
+m xóa alley à
+finnn[CODE]
+ — 10:45 CH
+m đã khai báo Lib chx
+Hoang°Minh°Hieu💤 — 10:45 CH
+hình như là
+chx
+lm sao v
+finnn[CODE]
+ — 10:46 CH
+code m cho t chx khai báo libs
+Hoang°Minh°Hieu💤 — 10:46 CH
+a lm dùm e vs
+finnn[CODE]
+ — 10:46 CH
+nhìn stream
+Hoang°Minh°Hieu💤 — 10:46 CH
+https://raw.githubusercontent.com/Henz-coder/Natri-hub-v2/refs/heads/main/Natri-hub.lua
+finnn[CODE]
+ — 10:47 CH
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/vinh12eqweaws/zingmods/refs/heads/main/zingpro')))()
+
+local Window = OrionLib:MakeWindow({
+    Name = "Natri-hub",
+    HidePremium = false,
+    SaveConfig = true,
+Mở rộng
+message.txt
+8 KB
+﻿
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/vinh12eqweaws/zingmods/refs/heads/main/zingpro')))()
+
+local Window = OrionLib:MakeWindow({
+    Name = "Natri-hub",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "dxl_bf"
+})
+
+local Tab = Window:MakeTab({
+    Name = "Scripts Mới",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 local Tab2 = Window:MakeTab({
-	Name = "Mod Client",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Mod Client",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 local Tab3 = Window:MakeTab({
-	Name = "Mod Game",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Mod Game",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
+
 local Tab4 = Window:MakeTab({
-	Name = "Script Cũ",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Script Cũ",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
+
+---
+
+-- Tab: Mod Game (Các script liên quan đến việc sửa đổi trong game)
 Tab3:AddButton({
-	Name = "RTX",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
+    Name = "RTX",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ để phản hồi nhanh hơn
         loadstring(game:HttpGet("https://pastebin.com/raw/vmjZ4UY8"))()
-end
+    end
 })
 
 Tab3:AddButton({
-	Name = "Fix Lag",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
-end
+    Name = "Fix Lag",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
+    end
 })
 
 Tab3:AddButton({
-	Name = "Anti Kick",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-     loadstring(game:HttpGet("https://pastebin.com/raw/FPfaukXN"))()
-end
+    Name = "Anti Kick",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://pastebin.com/raw/FPfaukXN"))()
+    end
 })
+
 Tab3:AddButton({
-	Name = "Farm Nhanh",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
+    Name = "Farm Nhanh",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
         loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/73mkp0aqyfo4ypy8hvl0nz10lq49fey5.lua"))()
-end
+    end
 })
 
+---
 
+-- Tab: Mod Client (Các script liên quan đến việc sửa đổi client hoặc executor)
 Tab2:AddButton({
-	Name = "Fruit Finder",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/AdminusGames/Blox_Fruits_Sniper/main/.lua'))()
-  	end    
-})
-
-Tab2:AddButton({
-	Name = "Arceus X",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
-end
+    Name = "Fruit Finder",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/AdminusGames/Blox_Fruits_Sniper/main/.lua'))() -- CẦN KIỂM TRA LẠI URL NÀY
+    end
 })
 
 Tab2:AddButton({
-	Name = "Krnl",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-      loadstring(game:HttpGet("https://pastebin.com/raw/M3HjLPU7"))()
-end
+    Name = "Arceus X",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
+    end
 })
 
 Tab2:AddButton({
-	Name = "Synapse X",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-      loadstring(game:HttpGet("https://pastebin.com/raw/QmqSgQ3K"))()
-end
+    Name = "Krnl",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://pastebin.com/raw/M3HjLPU7"))()
+    end
 })
 
 Tab2:AddButton({
-	Name = "Keyboard",
-	Callback = function()
-        repeat wait(5) until game:IsLoaded()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
-end
+    Name = "Synapse X",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://pastebin.com/raw/QmqSgQ3K"))()
+    end
+})
+
+Tab2:AddButton({
+    Name = "Keyboard",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Giảm thời gian chờ
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+    end
+})
+
+---
+
+-- Tab: Scripts Mới (Các script được thêm gần đây)
+Tab:AddButton({
+    Name = "NEW: Alchemy",
+    Callback = function()
+        -- Có thể cần thêm 'repeat wait(1) until game:IsLoaded()' nếu script yêu cầu game đã tải xong
+        loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
+    end
 })
 
 Tab:AddButton({
-	Name = "NEW: Alchemy",
-	Callback = function()
-       loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "NEW: Banana",
-	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/BananaHub/main/BananaHub.lua"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "NEW: BLUE X",
-	Callback = function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/EN.lua"))()
-  	end    
+    Name = "NEW: Banana",
+    Callback = function()
+        -- Có thể cần thêm 'repeat wait(1) until game:IsLoaded()' nếu script yêu cầu game đã tải xong
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/BananaHub/main/BananaHub.lua"))()
+    end
 })
 
 Tab:AddButton({
-	Name = "Chests Farm",
-	Callback = function()
+    Name = "NEW: BLUE X",
+    Callback = function()
+        -- Có thể cần thêm 'repeat wait(1) until game:IsLoaded()' nếu script yêu cầu game đã tải xong
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/EN.lua"))()
+    end
+})
+
+Tab:AddButton({
+    Name = "Chests Farm",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded() -- Thêm lại để đảm bảo game đã tải
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/ChestFarmOp"))()
-end    
+    end
 })
 
+---
 
+-- Tab: Script Cũ (Các script cũ hơn hoặc được lưu trữ)
 Tab4:AddButton({
-	Name = "Domadic Hub",
-	Callback = function()
+    Name = "Domadic Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Domadicoof/Domadicoof/main/Domadichub/NottoGay/Start.ranscript"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "FTS X Hub!",
-	Callback = function()
+    Name = "FTS X Hub!",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/tufreescript/FTS-X-Hub/main/FTSXHUB%20V1'))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "T7GFY GUI",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/minded102/dokinbog/main/README.md", true))()
-  	end    
+    Name = "T7GFY GUI",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/minded102/dokinbog/main/README.md", true))() -- CÓ THỂ KHÔNG PHẢI SCRIPT LUA
+    end
 })
 
 Tab4:AddButton({
-	Name = "CFrame Hub",
-	Callback = function()
+    Name = "CFrame Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Steveee11/Scripts/main/Blox%20Fruits%20OP%20Script"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Flare Hub",
-	Callback = function()
+    Name = "Flare Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/flares"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "BLCK Hub",
-	Callback = function()
+    Name = "BLCK Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/BLCK"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Modz HUb",
-	Callback = function()
+    Name = "Modz HUb",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/modz"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Hoho Hub",
-	Callback = function()
+    Name = "Hoho Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Lol Hub",
-	Callback = function()
+    Name = "Lol Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/neon"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Zen Hub",
-	Callback = function()
+    Name = "Zen Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader", true))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Uranium/Switch Hub",
-	Callback = function()
+    Name = "Uranium/Switch Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Augustzyzx/UraniumX/main/src_.lua.txt"))()
-  	end    
+    end
 })
 
-
 Tab4:AddButton({
-	Name = "Thunder Z",
-	Callback = function()
+    Name = "Thunder Z",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-05/HUB/main/Mobile-Loader"))()
-  	end    
+    end
 })
 
 Tab4:AddButton({
-	Name = "Mukuro Hub",
-	Callback = function()
+    Name = "Mukuro Hub",
+    Callback = function()
+        repeat wait(1) until game:IsLoaded()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
-  	end    
+    end
 })
+message.txt
+8 KB
